5f83f6d5f3a6f649c0a10f9352cf8c4cea916f6b
trix <farmazin@gmail.com>
2016-03-31 13:52:33 +0300

    fix autostart

--- configs/autostart.sh
+++ configs/autostart.sh
@@ -3,4 +3,7 @@ service ssh start
 service mysql start
 service apache2 start
 cd /var/www/ && tar -zxvf glpi-0.90.1.tar.gz
+rm /var/www/glpi-0.90.1.tar.gz
+rm -rf /var/www/html/
+mv /var/www/glpi /var/www/html
 mv /root/autostart /root/autostart.sh
