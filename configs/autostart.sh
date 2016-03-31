26cecd91d4bc48f1b30936e46743e2e2d8f1b41c
trix <farmazin@gmail.com>
2016-03-31 13:53:55 +0300

    fix

--- configs/autostart.sh
+++ configs/autostart.sh
@@ -6,4 +6,6 @@ cd /var/www/ && tar -zxvf glpi-0.90.1.tar.gz
 rm /var/www/glpi-0.90.1.tar.gz
 rm -rf /var/www/html/
 mv /var/www/glpi /var/www/html
+chown -R www-data:www-data /var/www/html
+chmod -R 770 /var/www/html
 mv /root/autostart /root/autostart.sh
